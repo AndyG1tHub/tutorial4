2f5444c73d22090fba8202d5aed2f9796d33c1e6
Andy <gmq0311@163.com>
2025-10-16 19:20:30 +0800

    changed test file

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -8,9 +8,9 @@ public class CalcTest {
 
     @Test
     void testAddition() {
-        assertEquals(4, c.add(2,2));
+        assertEquals(2, c.subtract(4,2));
     }
-    assertEquals(2, c.subtract(4,2));
+    
 
 }
  
